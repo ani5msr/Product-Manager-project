b6d9a8b5c290bd4d714731ee3cc8ada65f71e7a8
Anirudh Mishra <anirudh5mar@gmail.com>
2022-09-29 15:51:05 +0530

    Intial commit

---
+++ src/main/java/com/anirudh/productMan/ProductManApplication.java
@@ -0,0 +1,13 @@
+package com.anirudh.productMan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anirudh/productMan/ProductManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anirudh.productMan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
