6620df2c8c21080fea9c99ef20757d706f271edd
Anirudh Mishra <anirudh5mar@gmail.com>
2022-10-04 19:32:31 +0530

    Added Product class

--- src/main/java/com/anirudh/productMan/ProductManApplication.java
+++ src/main/java/com/anirudh/productMan/ProductManApplication.java
@@ -2,12 +2,14 @@ package com.anirudh.productMan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.slf4j.*;
 @SpringBootApplication
 public class ProductManApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(ProductManApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ProductManApplication.class, args);
+		logger.info("Started");
 	}
+	
 
 }
---
+++ src/main/java/com/anirudh/productMan/core/Product.java
@@ -0,0 +1,63 @@
+package com.anirudh.productMan.core;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String company, mfdDate, expDate, idNumber;
+	private int batchNo, price;
+	@Column(name="explanation", nullable=false, length=512)
+	private String description;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getMfdDate() {
+		return mfdDate;
+	}
+	public void setMfdDate(String mfdDate) {
+		this.mfdDate = mfdDate;
+	}
+	public String getExpDate() {
+		return expDate;
+	}
+	public void setExpDate(String expDate) {
+		this.expDate = expDate;
+	}
+	public String getIdNumber() {
+		return idNumber;
+	}
+	public void setIdNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+	public int getBatchNo() {
+		return batchNo;
+	}
+	public void setBatchNo(int batchNo) {
+		this.batchNo = batchNo;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
